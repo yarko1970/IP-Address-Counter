cbd6817da3ef4a8782f0e28957693a70ab700e28
yarko1970 <46659134+yarko1970@users.noreply.github.com>
2024-11-17 23:20:00 -0400

    File update

--- ip_address_counter.go
+++ ip_address_counter.go
@@ -11,6 +11,7 @@ import (
 	"time"
 )
 
+// generating bit mask for updating IP registry value
 func bitMask(bitPosition int) (int, int64) {
 	if bitPosition < 64 {
 		return 0, 1 << bitPosition
@@ -22,6 +23,7 @@ func bitMask(bitPosition int) (int, int64) {
 	return 3, 1 << (bitPosition - 192)
 }
 
+// calculating number of 1-bits in int64
 func numOnesInInt64(n int64) int {
 	var count int = 0
 	var tempInt int64 = n
@@ -36,6 +38,7 @@ func numOnesInInt64(n int64) int {
 	return count
 }
 
+// processing single line
 func lineProcessor(line string, ipRegistry *[256][256][256][4]int64) {
 	result := strings.Split(line, ".")
 	i0, _ := strconv.Atoi(result[0])
@@ -46,46 +49,52 @@ func lineProcessor(line string, ipRegistry *[256][256][256][4]int64) {
 	ipRegistry[i0][i1][i2][groupIndex] |= mask
 }
 
-func subBatchProcessor(lines []string, ipRegistry *[256][256][256][4]int64) {
-	if len(lines) == 0 {
+// processing sub-batch of lines
+func subBatchProcessor(subBatch []string, ipRegistry *[256][256][256][4]int64) {
+	if len(subBatch) == 0 {
 		return
 	}
-	for i := 0; i < len(lines); i++ {
-		lineProcessor(lines[i], ipRegistry)
+	for i := 0; i < len(subBatch); i++ {
+		lineProcessor(subBatch[i], ipRegistry)
 	}
 }
 
-func batchProcessor(lines []string, ipRegistry *[256][256][256][4]int64) {
+// processing batch of lines
+func batchProcessor(batch []string, ipRegistry *[256][256][256][4]int64) {
 
-	if len(lines) < 100 {
-		subBatchProcessor(lines, ipRegistry)
+	if len(batch) < 1000 {
+		subBatchProcessor(batch, ipRegistry)
 		return
 	}
 
 	var wg sync.WaitGroup
 
+	// setting the number of concurrent batch processes depending on available resources
 	var numOfProcs int = max(runtime.GOMAXPROCS(0)-1, 2)
 
+	// spawning and running concurrent sub-batch processing Goroutines
 	for i := 1; i < numOfProcs; i++ {
 		wg.Add(1)
 		go func(id int) {
 			defer wg.Done()
-			subBatch := lines[(len(lines)*(i-1))/numOfProcs : (len(lines)*i)/numOfProcs]
+			subBatch := batch[(len(batch)*(i-1))/numOfProcs : (len(batch)*i)/numOfProcs]
 			subBatchProcessor(subBatch, ipRegistry)
 		}(i)
 	}
 
-	subBatch := lines[(len(lines)*(numOfProcs-1))/numOfProcs : (len(lines)*numOfProcs)/numOfProcs]
+	subBatch := batch[(len(batch)*(numOfProcs-1))/numOfProcs : (len(batch)*numOfProcs)/numOfProcs]
 	subBatchProcessor(subBatch, ipRegistry)
 
 	wg.Wait()
 }
 
+// calculating the number of unique IPs
 func uniqueIpCount(filename string) int {
 
 	file, _ := os.Open(filename)
 	defer file.Close()
 
+	// creating and initializing IP registry
 	ipRegistry := [256][256][256][4]int64{}
 	for i0 := 0; i0 < 256; i0++ {
 		for i1 := 0; i1 < 256; i1++ {
@@ -106,6 +115,7 @@ func uniqueIpCount(filename string) int {
 	var batchIsReady bool = true
 	lines := []string{}
 
+	// scanning the file until complete
 	for scanner.Scan() {
 
 		var wg sync.WaitGroup
@@ -117,11 +127,14 @@ func uniqueIpCount(filename string) int {
 		batchIsReady = false
 
 		wg.Add(1)
+
+		// spawning Goroutine to process the ready batch
 		go func(id int) {
 			defer wg.Done()
 			batchProcessor(batch, &ipRegistry)
 		}(1)
 
+		// scanning the file to assemble next batch
 		for !batchIsReady {
 			ipAddress := scanner.Text()
 			lines = append(lines, ipAddress)
@@ -138,11 +151,13 @@ func uniqueIpCount(filename string) int {
 		wg.Wait()
 	}
 
+	// making sure leftover batch is processed
 	if len(lines) != 0 {
 		batchProcessor(lines, &ipRegistry)
 	}
 	fmt.Printf("Lines Scanned: %d\n", lineCounter)
 
+	// calculating the total number of 1-bits in IP registry
 	var ipCounter int = 0
 	for i0 := 0; i0 < 256; i0++ {
 		for i1 := 0; i1 < 256; i1++ {
